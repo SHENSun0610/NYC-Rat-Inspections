161216c8ac491b1767b8bcb50d2020ef7c4be1c2
xiekelly <kelly.xie@nyu.edu>
2017-07-18 21:55:39 -0400

    Spring 2017

--- NYCRatInspectionAnalysis.R
+++ NYCRatInspectionAnalysis.R
@@ -1,6 +1,6 @@
-# NYC Rat Inspection Analysis
-#
-# @author Kelly Xie
+# Kelly Xie
+# Prof. Tambe, ADE Spring 2017
+# Homework 1: NYC Rat Inspection Analysis
 
 
 ########################## PART 1A ##########################
@@ -12,12 +12,12 @@ library(dplyr)
 
 rodent = read.csv("~/Desktop/Kelly Y Xie")
 
-#parse inspection dates
+# parses inspection dates
 parseDate = parse_date_time(rodent$INSPECTION_DATE, c("%m/%d/%Y %H:%M:%S"))
 #charDates = as.character(as.Date(parseDate))
 as.Date(as.character(x$V1), format = "%m/%d/%Y")
 
-#add years and months to data frame
+# adds years and months to data frame
 year = substr(parseDate,1,4)
 rodent = cbind(rodent,year)
 
@@ -26,22 +26,22 @@ rodent = cbind(rodent,month)
 
 rodent$yearmonth = paste(rodent$year,rodent$month, sep="-") #year-month pairing
 
-#row of ones to keep track of count for every unique year-month pairing
+# adds row of ones to keep track of count for every unique year-month pairing
 rodent$ones = 1
 
-#get data for rat sightings in years 2011-2015
+# grabs data for rat sightings in years 2011-2015
 rodentFound = rodent[which(rodent$RESULT == "Active Rat Signs"),]
 rodentFound = subset(rodentFound, 
                      as.numeric(as.character(year))>2010 & as.numeric(as.character(year))<2016)
 
-#separate by borough
+# separates data by borough
 manhattan = subset(rodentFound, BOROUGH=="Manhattan")
 bronx = subset(rodentFound, BOROUGH=="Bronx")
 brooklyn = subset(rodentFound, BOROUGH=="Brooklyn")
 queens = subset(rodentFound, BOROUGH=="Queens")
 statenIsland = subset(rodentFound, BOROUGH=="Staten Island")
 
-#count up total sightings by borough for each year
+# counts up total sightings by borough for each year
 manhattanCount = manhattan %>% group_by(yearmonth) %>% 
   summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(yearmonth)
 bronxCount = bronx %>% group_by(yearmonth) %>% 
@@ -53,14 +53,14 @@ queensCount = queens %>% group_by(yearmonth) %>%
 statenCount = statenIsland %>% group_by(yearmonth) %>% 
   summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(yearmonth)
 
-#format all character dates to Date
+# formats all character dates to Date
 manhattanCount$yearmonth = as.Date(paste(manhattanCount$yearmonth,"01",sep="-"))
 bronxCount$yearmonth = as.Date(paste(bronxCount$yearmonth,"01",sep="-"))
 brooklynCount$yearmonth = as.Date(paste(brooklynCount$yearmonth,"01",sep="-"))
 queensCount$yearmonth = as.Date(paste(queensCount$yearmonth,"01",sep="-"))
 statenCount$yearmonth = as.Date(paste(statenCount$yearmonth,"01",sep="-"))
 
-#plot all boroughs on one chart
+# plots all boroughs on one chart
 gg = ggplot() + 
   geom_line(data=manhattanCount, aes(x=yearmonth, y=count, color='Manhattan')) +
   geom_line(data=bronxCount, aes(x=yearmonth, y=count, color='Bronx')) +
@@ -85,25 +85,25 @@ gg
 ########################## PART 1B ##########################
 
 
-#get data for all inspections in years 2011-2015
+# grabs data for all inspections in years 2011-2015
 rodentInspections = subset(rodent, 
                            as.numeric(as.character(year))>2010 & as.numeric(as.character(year))<2016)
 
-#separate by borough for rat sightings
+# separates by borough for rat sightings
 man = subset(rodentInspections, BOROUGH=="Manhattan" & RESULT=="Active Rat Signs")
 brx = subset(rodentInspections, BOROUGH=="Bronx" & RESULT=="Active Rat Signs")
 brk = subset(rodentInspections, BOROUGH=="Brooklyn" & RESULT=="Active Rat Signs")
 qns = subset(rodentInspections, BOROUGH=="Queens" & RESULT=="Active Rat Signs")
 stn = subset(rodentInspections, BOROUGH=="Staten Island" & RESULT=="Active Rat Signs")
 
-#separate by borough for total inspections
+# separates by borough for total inspections
 man2 = subset(rodentInspections, BOROUGH=="Manhattan")
 brx2 = subset(rodentInspections, BOROUGH=="Bronx")
 brk2 = subset(rodentInspections, BOROUGH=="Brooklyn")
 qns2 = subset(rodentInspections, BOROUGH=="Queens")
 stn2 = subset(rodentInspections, BOROUGH=="Staten Island")
 
-#count up rat sightings and total inspections by borough for each year
+# counts up rat sightings and total inspections by borough for each year
 manCount = man %>% group_by(yearmonth) %>% 
   summarise(active = sum(ones, na.rm=TRUE)) %>% arrange(yearmonth)
 brxCount = brx %>% group_by(yearmonth) %>% 
@@ -126,31 +126,31 @@ qnsCount2 = qns2 %>% group_by(yearmonth) %>%
 stnCount2 = stn2 %>% group_by(yearmonth) %>% 
   summarise(total = sum(ones, na.rm=TRUE)) %>% arrange(yearmonth)
 
-#join tables for each borough by year
+# joins tables for each borough by year
 manCount = inner_join(manCount,manCount2,by="yearmonth")
 brxCount = inner_join(brxCount,brxCount2,by="yearmonth")
 brkCount = inner_join(brkCount,brkCount2,by="yearmonth")
 qnsCount = inner_join(qnsCount,qnsCount2,by="yearmonth")
 stnCount = inner_join(stnCount,stnCount2,by="yearmonth")
 
-#calculate efficiency for each year:
-#efficiency for a given month is the number of inspections 
-#yielding "Active Rat Signs" in that month divided by 
-#the total number of inspections in that month
+# calculates efficiency for each year
+# Note: efficiency for a given month is the number of inspections 
+# yielding "Active Rat Signs" in that month divided by 
+# the total number of inspections in that month
 manCount$eff = manCount$active / manCount$total
 brxCount$eff = brxCount$active / brxCount$total
 brkCount$eff = brkCount$active / brkCount$total
 qnsCount$eff = qnsCount$active / qnsCount$total
 stnCount$eff = stnCount$active / stnCount$total
 
-#format all character dates to Date
+# formats all character dates to Date
 manCount$yearmonth = as.Date(paste(manCount$yearmonth,"01",sep="-"))
 brxCount$yearmonth = as.Date(paste(brxCount$yearmonth,"01",sep="-"))
 brkCount$yearmonth = as.Date(paste(brkCount$yearmonth,"01",sep="-"))
 qnsCount$yearmonth = as.Date(paste(qnsCount$yearmonth,"01",sep="-"))
 stnCount$yearmonth = as.Date(paste(stnCount$yearmonth,"01",sep="-"))
 
-#plot all boroughs on one chart
+# plots all boroughs on one chart
 gg = ggplot() + 
   geom_point(color="blue") + 
   geom_line(data=manCount, aes(x=yearmonth, y=eff, color='Manhattan')) +
@@ -176,69 +176,69 @@ gg
 ########################## PART 1C ##########################
 
 
-#get subset of data for inspections that yield active rat signs
+# grabs subset of data for inspections that yield active rat signs
 activeRat = subset(rodent, RESULT=="Active Rat Signs" &
                    as.numeric(as.character(year))>2010 & as.numeric(as.character(year))<2016)
 
-#get counts of active rat inspections for each unique zipcode
+# get counts of active rat inspections for each unique zipcode
 zipRat = activeRat %>% group_by(ZIP_CODE) %>% 
   summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count))
 
-#append corresponding boroughs
+# appends corresponding boroughs
 temp = subset(activeRat, select=c("ZIP_CODE","BOROUGH"))
-zipRat = unique(inner_join(zipRat,temp,by="ZIP_CODE") %>% group_by(ZIP_CODE)) #append boroughs
+zipRat = unique(inner_join(zipRat,temp,by="ZIP_CODE") %>% group_by(ZIP_CODE)) # appends boroughs
 
-#print top ten "hot spot" zipcodes among all 5 boroughs
+# prints top ten "hot spot" zipcodes among all 5 boroughs
 print(zipRat[1:10,])
 
 
 ########################## PART 2A ##########################
 
 
-#using the rodent inspection database
+# using the rodent inspection database
 
-#top 20 zip codes for rat activity prior to 2012
+# top 20 zip codes for rat activity prior to 2012
 rodentsBefore = subset(rodent, RESULT=="Active Rat Signs" &
                          as.numeric(as.character(year))<2012)
 topBefore = rodentsBefore %>% group_by(ZIP_CODE) %>% 
-  summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count)) #get counts for unique zipcodes
+  summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count)) # get counts for unique zipcodes
 
 tempBefore = subset(rodentsBefore, select=c("ZIP_CODE","BOROUGH"))
-topBefore = unique(inner_join(topBefore,tempBefore,by="ZIP_CODE") %>% group_by(ZIP_CODE)) #append boroughs
+topBefore = unique(inner_join(topBefore,tempBefore,by="ZIP_CODE") %>% group_by(ZIP_CODE)) # appends boroughs
 print(topBefore[1:20,])
 
-#top 20 zip codes for rat activity after 2012
+# top 20 zip codes for rat activity after 2012
 rodentsAfter = subset(rodent, RESULT=="Active Rat Signs" &
                         as.numeric(as.character(year))>2012)
 topAfter = rodentsAfter %>% group_by(ZIP_CODE) %>% 
-  summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count)) #get counts for unique zipcodes
+  summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count)) # get counts for unique zipcodes
 
 tempAfter = subset(rodentsAfter, select=c("ZIP_CODE","BOROUGH"))
-topAfter = unique(inner_join(topAfter,tempAfter,by="ZIP_CODE") %>% group_by(ZIP_CODE)) #append boroughs
+topAfter = unique(inner_join(topAfter,tempAfter,by="ZIP_CODE") %>% group_by(ZIP_CODE)) # appends boroughs
 print(topAfter[1:20,])
 
-#top 20 zip codes for rat activity during Hurricane Sandy
-sandy = read.csv("~/Desktop/sandyrelated.csv") #Hurricane Sandy 311 database
+# top 20 zip codes for rat activity during Hurricane Sandy
+sandy = read.csv("~/Desktop/sandyrelated.csv") # Hurricane Sandy 311 database
 sandy = subset(sandy, Complaint.Type=="Rodent" & Descriptor!="Condition Attracting Rodents")
 sandy$ones = 1
 topSandy = sandy %>% group_by(Incident.Zip) %>% 
-  summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count)) #get counts for unique zipcodes
+  summarise(count = sum(ones, na.rm=TRUE)) %>% arrange(desc(count)) # get counts for unique zipcodes
 tempSandy = subset(sandy, select=c("Incident.Zip","Borough"))
-topSandy = unique(inner_join(topSandy,tempSandy,by="Incident.Zip") %>% group_by(Incident.Zip)) #append boroughs
+topSandy = unique(inner_join(topSandy,tempSandy,by="Incident.Zip") %>% group_by(Incident.Zip)) # appends boroughs
 print(topSandy[1:20,])
 
 
 ########################## PART 3 ##########################
 
 
-#restaurant inspection database
+# restaurant inspection database
 restaurant = read.csv("~/Desktop/DOHMH_New_York_City_Restaurant_Inspection_Results.csv")
 
-#STEP 1
-#variable for year-month-zipcode combination
+# STEP 1
+# variable for year-month-zipcode combination
 rodent$yearmonthzip = paste(rodent$year,rodent$month,rodent$ZIP_CODE,sep="-")
 
-#get rat sightings and total inspections
+# grabs rat sightings and total inspections
 ratSightings = filter(rodent,RESULT=="Active Rat Signs" & 
                         as.numeric(as.character(year))>2009 & 
                         as.numeric(as.character(year))<2016) %>% 
@@ -249,51 +249,52 @@ totalInspections = filter(rodent,
                          as.numeric(as.character(year))<2016) %>% 
   group_by(yearmonthzip) %>% summarise(total = sum(ones, na.rm=TRUE)) %>% arrange(yearmonthzip)
 
-#outer join two tables and compute likelihood
+# outer joins two tables and compute likelihood
 ratSightings = full_join(ratSightings,totalInspections,by="yearmonthzip")
 activeRatSightings = (ratSightings$rat / ratSightings$total)
 likelihood = as.data.frame(cbind(yearmonthzip = ratSightings$yearmonthzip,activeRatSightings))
-#activeRatSightings is predictor variable: the likelihood of active rat signs
-#in the same zip code as the restaurant, and in the same month and year 
-#in which the restaurant was inspected
+
+# Note: activeRatSightings is predictor variable: the likelihood of active rat signs
+# in the same zip code as the restaurant, and in the same month and year 
+# in which the restaurant was inspected
 
 
-#STEP 2
-#merge likelihood with restaurant violation data
+# STEP 2
+# merges likelihood with restaurant violation data
 mth = substr(as.character(restaurant$INSPECTION.DATE),1,2)
 yr = substr(as.character(restaurant$INSPECTION.DATE),7,10)
 restaurant$yearmonthzip = paste(yr, mth, restaurant$ZIPCODE, sep="-")
-restaurant = full_join(restaurant,likelihood,by="yearmonthzip") #outer join
+restaurant = full_join(restaurant,likelihood,by="yearmonthzip") # outer join
 
-#convert all NA to 0 values for activeRatSightings
+# converts all NA to 0 values for activeRatSightings
 restaurant$activeRatSightings = as.numeric(as.character(restaurant$activeRatSightings))
 restaurant$activeRatSightings[is.na(restaurant$activeRatSightings)] <- 0
 
 
-#STEP 3
-#set seed
+# STEP 3
+# sets seed
 set.seed(4698)
 
 
-#STEP 4
-#binary variable ratViolation that takes the value 1 or 0 
-#depending on whether a restaurant violation is due to rat problems
+# STEP 4
+# binary variable ratViolation that takes the value 1 or 0 
+# depending on whether a restaurant violation is due to rat problems
 restaurant = mutate(restaurant, ratViolation = as.numeric(VIOLATION.CODE == "04L" | 
                                         VIOLATION.CODE == "04K" | 
                                         VIOLATION.CODE == "08A"))
 
-#convert month and year to factor variables
+# converts month and year to factor variables
 restaurant$year = factor(substr(restaurant$yearmonthzip,1,4))
 restaurant$month = factor(substr(restaurant$yearmonthzip,6,7))
 
-#get subset of data for years 2012-2015
+# gets subset of data for years 2012-2015
 restaurant = restaurant %>% subset(as.numeric(as.character(year)>2011) &
                                      as.numeric(as.character(year)<2016))
 
 
-#run a logistic regression on merged data set to test 
-#activeRatSightings variable has a statistically significant relationship 
-#to ratViolation predictor
+# runs a logistic regression on merged data set to test 
+# activeRatSightings variable has a statistically significant relationship 
+# to ratViolation predictor
 fit = glm(ratViolation ~ activeRatSightings + month + year, data=restaurant, family=binomial)
 summary(fit)
 
